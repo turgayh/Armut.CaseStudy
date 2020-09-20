9d7122ea3c37c47015459445f3bb1cb224672f52
hakan turğay <turgayh@itu.edu.tr>
2020-09-20 19:47:42 +0300

    unit test

---
+++ tests/Armut.CaseStudy.UnitTests/Services/UserServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Armut.CaseStudy.Operation.UserServices;
+using Moq;
+using Xunit;
+
+namespace Armut.CaseStudy.UnitTests.Services
+{
+
+    public class UserServiceTests 
+    {
+        private readonly IMock<IUserService> mockService;
+        public UserServiceTests(IMock<IUserService> mock)
+        {
+            mockService = mock;
+        }
+
+        [Fact]
+        public void Login_NullParameter_ReturnErrorMessage()
+        {
+
+        }
+
+    }
+}
